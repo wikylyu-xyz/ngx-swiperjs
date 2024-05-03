4bdf3887be8b96ebec55c7bb34f524f394d1375c
Wiky Lyu <wikylyu@outlook.com>
2024-05-03 10:05:28 +0800

    nullcheck

--- projects/ngx-swiperjs/src/lib/directives/swiper.directive.ts
+++ projects/ngx-swiperjs/src/lib/directives/swiper.directive.ts
@@ -16,12 +16,15 @@ export class SwiperDirective {
   }
 
   ngAfterViewInit() {
+    if (!this.el.nativeElement) {
+      return;
+    }
     Object.assign(this.el.nativeElement, this.config);
 
     // @ts-ignore
-    this.el.nativeElement.initialize();
+    this.el.nativeElement?.initialize();
     // @ts-ignore
-    if (this.el.nativeElement.swiper.autoplay) {
+    if (this.el.nativeElement?.swiper?.autoplay) {
       // @ts-ignore
       this.el.nativeElement.swiper.autoplay.start();
     }
