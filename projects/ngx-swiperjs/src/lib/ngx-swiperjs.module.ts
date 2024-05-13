cca2c4d58a62e017ac49ee5f0fa2ce63999eaeb9
Wiky Lyu <wikylyu@outlook.com>
2024-05-13 14:10:04 +0800

    fix BrowserModule

--- projects/ngx-swiperjs/src/lib/ngx-swiperjs.module.ts
+++ projects/ngx-swiperjs/src/lib/ngx-swiperjs.module.ts
@@ -6,11 +6,12 @@ import {
   SwiperComponent,
 } from './components/swiper/swiper.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 register();
 @NgModule({
   declarations: [SwiperDirective, SwiperComponent, SlideDirective],
-  imports: [BrowserModule],
+  imports: [CommonModule],
   exports: [SwiperDirective, SwiperComponent, SlideDirective],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
